70247b6c790b1b5d9a443333241c1db79271d809
Pere Giménez <gimenezfebrer@gmail.com>
2023-08-15 15:58:20 +0200

    moved code out of routes

--- app.jl
+++ app.jl
@@ -1,24 +1,19 @@
 module App
 using GenieFramework
-using Genie.Requests
 include("lib/StatisticAnalysis.jl")
 include("controllers/AnalysisController.jl")
 using .StatisticAnalysis
 using .AnalysisController
 
-route("/analysis", AnalysisController.analysis)
+route("/", AnalysisController.index)
 
 route("/form") do
     html(Renderer.filepath("pages/form.jl.html"))
 end
 
-route("/form", method=POST) do
-    N = parse(Int, postpayload(:N, 20))
-    AnalysisController.analysis(N)
-end
+route("/form", AnalysisController.analysis,  method=POST)
 
-route("/numbers/:N::Int") do
-    AnalysisController.numbers(payload(:N))
-end
+route("/numbers/:N::Int", AnalysisController.numbers)
+
 
 end
--- controllers/AnalysisController.jl
+++ controllers/AnalysisController.jl
@@ -1,17 +1,22 @@
 module AnalysisController
-using Genie.Renderer
-using Genie.Renderer.Html
-
+using GenieFramework.Genie.Renderer.Html
+using GenieFramework.Genie.Requests
 using .Main.App.StatisticAnalysis
 
-function analysis(N=20)
-    x = gen_numbers(N)
-    m = sum(x / N)
-    html(Renderer.filepath("pages/analysis.jl.html"), m=m)
+function index()
+    html("<h1>Welcome to the number analysis module</h1>
+          <a href=/form>Numbers form</a>")
 end
 
-function numbers(N=20)
-    html(Renderer.filepath("pages/numbers.jl.html"), x=gen_numbers(N))
+function analysis()
+    N = parse(Int, postpayload(:N, 20))
+    x = gen_numbers(N)
+    m = sum(x / N)
+    html(Renderer.filepath("pages/analysis.jl.html"), N=N, m=m)
+end
+
+function numbers()
+    html(Renderer.filepath("pages/numbers.jl.html"), x=gen_numbers(payload(:N)))
 end
 
 end
